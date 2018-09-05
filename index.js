dfe72fbae075c4329c23960bdd0c7fc68679507b
derrick b <dbozich1@gmail.com>
2018-09-05 08:51:02 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -3,10 +3,10 @@
 function scuberGreetingForFeet(value){
   let charge;
   if (value <= 400) {
-    charge = "This one is on me!"
+    charge = "This one is on me!";
   } else if (value > 2000) {
-    charge = "I will gladly take your thirty bucks."
+    charge = "I will gladly take your thirty bucks.";
   } else if (value > 2500)  {
-    
+    charge = 'No can do.';
   }
 }
